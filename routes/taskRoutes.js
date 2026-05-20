c9ea00b2f06eff04d7b54050e545e8577f35a200
malak <moussatimalak@gmail.com>
2026-05-20 07:18:55 -0700

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Task = require('../models/Task');
 
 router.post('/taches', async (req, res) => {
-    try {
+  try {
         const nouvelleTache = new Task(req.body);
         await nouvelleTache.save();
         res.status(201).json(nouvelleTache);
@@ -11,16 +11,6 @@ router.post('/taches', async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 });
-
-router.get('/taches', async (req, res) => {
-    try {
-        const taches = await Task.find().populate('assigneA', 'nom email');
-        res.status(200).json(taches);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
 router.put('/taches/:id', async (req, res) => {
     try {
         const tacheModifiee = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
