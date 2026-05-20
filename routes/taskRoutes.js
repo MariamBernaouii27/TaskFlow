4984a44cf2f1e8a4ca16a6565f2ba9f770939ebd
malak <moussatimalak@gmail.com>
2026-05-20 07:24:02 -0700

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -20,23 +20,4 @@ router.get('/taches', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
-
-router.put('/taches/:id', async (req, res) => {
-    try {
-        const tacheModifiee = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.status(200).json(tacheModifiee);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-router.delete('/taches/:id', async (req, res) => {
-    try {
-        await Task.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: 'Tâche supprimée avec succès' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
 module.exports = router;
